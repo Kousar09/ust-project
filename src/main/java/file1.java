1db269274857f1d480ce928ac8587f301e88a84e
Kousar <kousarshaik125@gmail.com>
2024-02-22 21:10:46 +0530

    file1.java is added

---
+++ src/main/java/file1.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+
+import com.example.sharathproject.SharathprojectApplication;
+
+public class file1 {
+    public static void main(String[] args) {
+        SpringApplication.run(SharathprojectApplication.class, args);
+    }
+}
