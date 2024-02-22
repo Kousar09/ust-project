1fbdf54c75072ca831591ac60fec596a6d7e5c27
Kousar <kousarshaik125@gmail.com>
2024-02-22 20:44:20 +0530

    first commit

---
+++ src/main/java/com/example/sharathproject/SharathprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharathproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharathprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharathprojectApplication.class, args);
+	}
+
+}
